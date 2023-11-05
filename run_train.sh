9d4154b7d1d13600bfb41b696e99ca9ab7e56ced
samithuang <285365963@qq.com>
2023-11-05 11:44:35 +0800

    ms1024 fscore 83.8, add files

--- run_train.sh
+++ run_train.sh
@@ -1,10 +1,9 @@
-# export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE" # debugging
+#export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE" # debugging
+export MS_ASCEND_CHECK_OVERFLOW_MODE=1  # debugging
 export DEVICE_ID=$1
 
-#task_name=train_db_r50_1p_momentum # rewrite
 yaml_file=configs/det/dbnet/db_r50_icdar15.yaml # rewrite
-task_name=train_db_r50_ic15_allowfp32tofp16_ls65536 #rewrite
-#yaml_file=configs/det/dbnet/db_r50_td500.yaml # rewrite
+task_name=train_db_r50_ic15_aff_ls65536_lsInpFp32_val2 #rewrite
 output_path=outputs
 
 rm -rf ${output_path:?}/${task_name:?}
