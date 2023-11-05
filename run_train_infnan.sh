f5eb2bce9aa623a5793b641bb7ab85e04554fd68
samithuang <285365963@qq.com>
2023-11-05 20:57:51 +0800

    add config files for infnan fscore ok version

---
+++ run_train_infnan.sh
@@ -0,0 +1,19 @@
+export MS_ASCEND_CHECK_OVERFLOW_MODE="INFNAN_MODE" # debugging
+#export MS_ASCEND_CHECK_OVERFLOW_MODE=1  # set to avoid INFNAN dy default
+export DEVICE_ID=$1
+
+yaml_file=configs/det/dbnet/db_r50_icdar15_infnan.yaml # rewrite
+task_name=train_db_r50_ic15_infnan_staticLs4096 #rewrite
+output_path=outputs
+
+rm -rf ${output_path:?}/${task_name:?}
+mkdir -p ${output_path:?}/${task_name:?}
+
+# remove files
+output_dir=$output_path/$task_name
+cp $0 $output_dir/.
+
+python -u tools/train.py \
+    --config=$yaml_file  \
+    -o train.ckpt_save_dir=$output_dir eval.ckpt_load_path=$output_dir/best.ckpt system.distribute=False \
+    > $output_dir/train.log 2>&1 &
